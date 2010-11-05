40a9495db5320d55c8bbba845312b404725ab370
Jake <jake@jake-desktop.(none)>
2010-11-04 23:09:52 -0700

    Closures

--- JScript/Sun.js
+++ JScript/Sun.js
@@ -1,127 +1,126 @@
-  
-  
-  function Tzunit() {
-      this.TestCases = [];
-      this.UnidentifiedTestId = 1;   
-      this.SuccessCount = 0; 
-      this.TestDescription = "";
-      this.writer = new Writer();
-  };
-    
-    Tzunit.prototype.Actual = function(act) { 
-      this.ActualValue = act; 
-      return this; 
-    };
-    
-    Tzunit.prototype.Expected = function(exp) { 
-      this.ExpectedValue = exp; 
-      return this; 
-    };
-    
-    Tzunit.prototype.IsNull = function(act) { 
-      this.Evaluation = function() { return act == null || act === 'undefined'; }
-      this.TestDescription = this.Evaluation() ? "Object Was Null" : "Object Was Not Null";
-      return this; 
-    };
-    
-    Tzunit.prototype.NotNull = function(act) { 
-      this.Evaluation = function() { return act != null && act !== 'undefined'; }
-      this.TestDescription = this.Evaluation() ?  "Object Was Not Null" : "Object Was Null";
-      return this; 
-    };
-    
-    Tzunit.prototype.Assert = function(act, exp) { 
+var Tzu = (function() {  
+        var TestCases = [],
+            UnidentifiedTestId = 1,   
+            SuccessCount = 0,
+            TestDescription = "",
+            Write = Writer,
+            ExpectedValue = 0,
+            ActualValue = 0,
+            Evaluation = function(){};   
+       
+        var containsSubstring = function (col, item) {
+              if(col.indexOf(item) == -1) { return false; }      
+              return true;      
+        };
+                            
+        var containsElement = function (col, item) {
+            for(var i=0;i<col.length;i++)
+             { if(col[i] == item) { return true; }  }
+            return false;
+        };
+              
+        var arraysMatch = function (act, exp) {
+             if (act.length != exp.length) { return function () { return false; }; }
+             else { return function () {
+                for(var i=0;i<act.length;i++)
+                { if(act[i] != exp[i]) { return false; } }
+              };
+       }
+     }
         
-            var arraysMatch = function (act, exp) {
-              if (act.length != exp.length) return function () { return false; };
-              else return function () {
-                 for(var i=0;i<act.length;i++)
-                 { if(act[i] != exp[i]) { return false; } 
-              }
-              return true;
-              }
-            }            
-      if(act[0] == 'undefined' && typeof act.splice != 'undefined'){
-         this.Evaluation = arraysMatch(act, exp);
-         this.TestDescription = (this.Evaluation()) ? "Two Collections Match" : "Two Collections Do Not Match";
+  return {      
+    Actual : function(act) { 
+      ActualValue = act; 
+      return this; 
+    },
+    
+    Expected : function(exp) { 
+      ExpectedValue = exp; 
+      return this; 
+    },
+    
+    IsNull : function(act) { 
+      Evaluation = function() { return act === null || act === 'undefined'; };
+      TestDescription = Evaluation() ? "Object Was Null" : "Object Was Not Null";
+      return this; 
+    },
+    
+     NotNull : function(act) { 
+      Evaluation = function() { return act !== null && act !== 'undefined'; };
+      TestDescription = Evaluation() ?  "Object Was Not Null" : "Object Was Null";
+      return this; 
+    },
+    
+    Assert : function(act, exp) { 
+      if(act[0] == 'undefined' && typeof act.splice != 'undefined') {
+         Evaluation = arraysMatch(act, exp);
+         TestDescription = (Evaluation()) ? "Two Collections Match" : "Two Collections Do Not Match";
          return this;
       }
-      this.Evaluation = function() { return act == exp; }
-      this.TestDescription = (this.Evaluation()) ? "Objects are equal" : "Objects are not equal";
+      Evaluation = function() { return act == exp; };
+      TestDescription = (Evaluation()) ? "Objects are equal" : "Objects are not equal";
       return this; 
-    };
+    },
     
-    Tzunit.prototype.Contains = function(collection, item) {
-      
-              var containsSubstring = function (col, item) {
-                 if(col.indexOf(item) == -1) { return false; }      
-                 return true;      
-              };
-                            
-              var containsElement=function (col, item) {
-                 for(var i=0;i<col.length;i++)
-                  { if(col[i] == item) { return true; }  }
-                 return false;
-              }
-        if(typeof collection == 'string') this.Evaluation = function() { return containsSubstring(collection, item); }
-        if(typeof collection.splice != 'undefined') this.Evaluation = function() { return containsElement(collection, item); }    
-        this.TestDescription = (this.Evaluation()) ? item + " was found" : item +" could not be found";
+    Contains : function(collection, item) {
+        if(typeof collection == 'string') { Evaluation = function() { return containsSubstring(collection, item); }; }
+        if(typeof collection.splice != 'undefined') { Evaluation = function() { return containsElement(collection, item); }; } 
+        TestDescription = (Evaluation()) ? item + " was found" : item +" could not be found";
         return this;
-    };
+    },
     
-    Tzunit.prototype.NotEqual = function(act, expected) {
-      this.Evaluation = function() { return act != expected; }
-      this.TestDescription = (this.Evaluation()) ? "Objects are not equal" : "Objects are equal";
+    NotEqual : function(act, expected) {
+      Evaluation = function() { return act != expected; };
+      TestDescription = (Evaluation()) ? "Objects are not equal" : "Objects are equal";
       return this;   
-    };
+    },
     
-    Tzunit.prototype.OfSameType = function(act, expected) {
-      this.Evaluation = function() { return typeof act == typeof expected; }
-      this.TestDescription = (this.Evaluation()) ? "Objects are of same type" : "Objects are not of same type";
+    OfSameType : function(act, expected) {
+      Evaluation = function() { return typeof act == typeof expected; };
+      TestDescription = (Evaluation()) ? "Objects are of same type" : "Objects are not of same type";
       return this;   
-    };    
+    },  
     
-    Tzunit.prototype.NotOfSameType = function(act, expected) {
-      this.Evaluation = function() { return typeof act != typeof expected; }
-      this.TestDescription = (this.Evaluation()) ? "Objects are not of same type" : "Objects are of same type" ;
+    NotOfSameType : function(act, expected) {
+      Evaluation = function() { return typeof act != typeof expected; };
+      TestDescription = (Evaluation()) ? "Objects are not of same type" : "Objects are of same type" ;
       return this;   
-    };
-    
-    Tzunit.prototype.WithoutException = function(func) {
-      
-      this.Evaluation = function() { try { func() } catch(er) { return false; } return true; }
-      this.TestDescription = (this.Evaluation()) ? "Function Executed Without a Hitch" : "Function Threw up an exception" ;
+    },
+   
+    WithoutException : function(func) {      
+      Evaluation = function() { try { func(); } catch(er) { return false; } return true; };
+      TestDescription = (Evaluation()) ? "Function Executed Without a Hitch" : "Function Threw up an exception" ;
       return this;   
-    };
+    },
     
     /* Rendering / Handling */
     
-    Tzunit.prototype.Test = function(name) {
-      if(name == null) name = "Unidentified Test #" + this.UnidentifiedTestId++;
+    Test : function(name) {
+      if(name == null || typeof name == 'undefined') { name = "Unidentified Test #" + UnidentifiedTestId++; }
       var desc = "";
-      if(this.Evaluation == null || typeof this.Evaluation == 'undefined') {
-        this.TestDescription = "Actual: "+ this.ActualValue + " <-> Expected: " + this.ExpectedValue;
-        this.Evaluation = function () { return this.ActualValue == this.ExpectedValue; } 
+      if(Evaluation == null || typeof Evaluation == 'undefined') {
+        TestDescription = "Actual: "+ ActualValue + " <-> Expected: " + ExpectedValue;
+        Evaluation = function () { return ActualValue == ExpectedValue; }; 
       }
-      this.TestCases.push(new TestCase(name, this.TestDescription, this.Evaluation));
-    };
+      TestCases.push(new TestCase(name, TestDescription, Evaluation));
+    },
     
-    Tzunit.prototype.RenderTests = function() {      
-      this.writer.WriteDivs();
-        for(var i=0; i < this.TestCases.length; i++) {
-           if(this.TestCases[i].Evaluation()) this.HandleSuccess(i);
-           else this.HandleFail(i);
+    RenderTests : function() {      
+      Write.WriteDivs();
+        for(var i=0; i < TestCases.length; i++) {
+           if(TestCases[i].Evaluation()) { this.HandleSuccess(i);} 
+           else { this.HandleFail(i); }
          }       
-       new Effect(this.TestCases).Apply(this.TestCases.length -this.SuccessCount, this.writer.HeadText(this.SuccessCount, this.TestCases.length));
-    };
+       new Effect(TestCases).Apply(TestCases.length - SuccessCount, Write.HeadText(SuccessCount, TestCases.length));
+    },
    
-   Tzunit.prototype.HandleSuccess = function(id) {
-       this.SuccessCount++;
-       this.writer.WriteSuccess(this.TestCases[id]);
-   };
+   HandleSuccess : function(id) {
+       SuccessCount++;
+       Write.WriteSuccess(TestCases[id]);
+   },
    
-   Tzunit.prototype.HandleFail = function(id) {
-      this.writer.WriteFail(this.TestCases[id]);
-   };
-   
-var Tzu = new Tzunit();    
+   HandleFail : function(id) {
+      Write.WriteFail(TestCases[id]);
+    }
+  };
+})();
--- JScript/Writer.js
+++ JScript/Writer.js
@@ -1,39 +1,40 @@
-function Writer() {
-   this.caseNum = 1;
- };      
-
-    Writer.prototype.WriteToTest = function (message, result) {
-          $("<li id=" +this.caseNum +" class=testCase></li>").appendTo("#cases");
-          $("<p class=message id=H"+this.caseNum+" href=#>" + message + "</p>").appendTo("#"+this.caseNum);
-          $("<li id=hover"+this.caseNum+" ><li/>").insertAfter("#"+this.caseNum);
-    };
+var Writer = (function () {
+   var caseNum = 1;   
+return {
+   WriteToTest : function (message, result) {
+          $("<li id=" +caseNum +" class=testCase></li>").appendTo("#cases");
+          $("<p class=message id=H"+caseNum+" href=#>" + message + "</p>").appendTo("#"+caseNum);
+          $("<li id=hover"+caseNum+" ><li/>").insertAfter("#"+caseNum);
+    },
         
-    Writer.prototype.HeadText = function(success, total) {
+   HeadText : function(success, total) {
         var message = success + " of " + total + " Tests Passed";
         $("#resultText").html(message);
-    };
+    },
     
-    Writer.prototype.WriteDivs = function() {
+   WriteDivs : function() {
             $('<section id="cases">').appendTo('#TestCase');
             $("</section>").appendTo('#TestCase');
-    };
+    },
         
-    Writer.prototype.WriteSuccess = function(testCase) {
+   WriteSuccess : function(testCase) {
             this.WriteToTest(testCase.Name, true);
-            $("#"+this.caseNum).css("background-color","green");
-            $("#"+this.caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, green 0%,  #C0C0C0  100%)");
-            $("#"+this.caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(green), to(#C0C0C0 ))");
-            $("#hover"+this.caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, #ffffff 0%, green 100%");
-            $("#hover"+this.caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(#ffffff), to(green)");
-            this.caseNum++;
-    };   
+            $("#"+caseNum).css("background-color","green");
+            $("#"+caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, green 0%,  #C0C0C0  100%)");
+            $("#"+caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(green), to(#C0C0C0 ))");
+            $("#hover"+caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, #ffffff 0%, green 100%");
+            $("#hover"+caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(#ffffff), to(green)");
+            caseNum++;
+    },  
     
-    Writer.prototype.WriteFail = function(testCase) {
+   WriteFail : function(testCase) {
             this.WriteToTest(testCase.Name, false);
-            $("#"+this.caseNum).css("background-color", "red");
-            $("#"+this.caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, red 0%,  #C0C0C0  100%)");
-            $("#"+this.caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(red), to(#C0C0C0 ))");
-            $("#hover"+this.caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, #ffffff 0%, red 100%");
-            $("#hover"+this.caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(#ffffff), to(red)");
-            this.caseNum++;
-    };
+            $("#"+caseNum).css("background-color", "red");
+            $("#"+caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, red 0%,  #C0C0C0  100%)");
+            $("#"+caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(red), to(#C0C0C0 ))");
+            $("#hover"+caseNum).css("background", "-moz-radial-gradient(center 45deg, circle closest-corner, #ffffff 0%, red 100%");
+            $("#hover"+caseNum).css("background", "-webkit-gradient(radial, center center, 0, center center, 70, from(#ffffff), to(red)");
+            caseNum++;
+    }
+  };
+})();
